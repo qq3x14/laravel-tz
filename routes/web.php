afecbd6a8631d2496b937514c744437e5be1269f
Dmitry <qq3x14@gmail.com>
2024-02-22 20:16:47 +0500

    Add routes.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ShopCartsController;
+use App\Http\Controllers\ShopOrdersController;
+use App\Http\Controllers\ShopProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ShopProductsController::class, 'list'])->name('products.list');
+
+Route::get('/cart', [ShopCartsController::class, 'show'])->name('cart.show');
+Route::post('/cart', [ShopCartsController::class, 'add'])->name('cart.add');
+
+Route::get('/orders', [ShopOrdersController::class, 'list'])->name('orders.list');
+Route::get('/order/create', [ShopOrdersController::class, 'create'])->name('order.create');
+Route::get('/order/{id}/delete', [ShopOrdersController::class, 'delete'])->name('order.delete')->where('id', '[0-9]+');
